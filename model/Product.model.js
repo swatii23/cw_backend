6c8779ec110be52583fac0990f7bc8054abaca4c
Swati Mandal <swati.m_2000@rediffmail.com>
2024-01-05 17:50:02 +0530

    product model defined

--- model/Product.model.js
+++ model/Product.model.js
@@ -1,7 +1,60 @@
 const { mongoose } = require("../imports/module.import")
 
 const productSchema = mongoose.Schema({
-    
+    category: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    currency: {
+        type: String,
+        default: "INR"
+    },
+    material: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    review: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    description: [
+        {
+            short: {
+                type: String,
+                required: true
+            },
+            long: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    images: {
+        type: [String],
+        required: true
+    }
 })
 
-module.exports = {  }
+const Productmodel = mongoose.model("product", productSchema)
+
+module.exports = { Productmodel }
