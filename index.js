b635a27186214072c1b0f763aa7068bf647602e4
Swati Mandal <swati.m_2000@rediffmail.com>
2024-01-08 16:36:01 +0530

    cors updated

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: "*",
+        origin: ["http://127.0.0.1:5173"], // Add your frontend origin(s) here
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "Authorization"],
     }
 ))
 
