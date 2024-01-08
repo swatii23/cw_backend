282eddb49c8ee1aee5a4247c745f4d9b61f68f16
Swati Mandal <swati.m_2000@rediffmail.com>
2024-01-08 21:43:37 +0530

    cors

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const cors = require("cors")
 const app = express()
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors(
+    {
+        origin: "*"
+    }
+))
 
 app.use("/auth", authRouter)
 app.use("/products", productRouter)
