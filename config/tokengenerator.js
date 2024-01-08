e6798c4c85006e3b3101a4d2b00a535210238f9c
Swati Mandal <swati.m_2000@rediffmail.com>
2024-01-08 13:10:12 +0530

    token key renamed

--- config/tokengenerator.js
+++ config/tokengenerator.js
@@ -1,7 +1,7 @@
 const { jwt } = require("../imports/module.import")
 
 const token = (id) => {
-     return jwt.sign({ userId: id }, process.env.PRIVATE_KEY);
+     return jwt.sign({ userId: id }, process.env.SECRET_KEY);
 }
 
 module.exports = { token }
