7fc7ae06660e9f2c2c154cc90e436c67cf1e4beb
Swati Mandal <swati.m_2000@rediffmail.com>
2024-01-08 20:14:58 +0530

    bug fix

--- imports/modules.imports.js
+++ imports/modules.imports.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-// const colors = require("colors")
 const asyncHandler = require("express-async-handler")
 require("dotenv").config()
 
